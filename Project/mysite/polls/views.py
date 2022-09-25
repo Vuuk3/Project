ec205b1bbc62f9ad9c0864aa397f96612a9afad1
Vuuk3 <99895368+Vuuk3@users.noreply.github.com>
2022-09-25 19:02:23 +0300

    Update views.py

--- Project/mysite/polls/views.py
+++ Project/mysite/polls/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 def str1(request):
     return HttpResponse("<p>Привет, меня зовут Тюпин Даниил. Я из Москвы.<p>"
-                        "<p>Благодаря Яндекс Лицею я имею 2 года опыта программмирования на Python.<p>"
+                        "<p>Благодаря Лицею академии Яндекса я имею 2 года опыта программмирования на Python.<p>"
                         "<p>Помимо программирования я увлекаюсь кулинарией, чтением книг и спортом<p>"
                         '<div>'
                         '<img src="https://raw.githubusercontent.com/Vuuk3/Project/main/1664117970700.jpg" '
